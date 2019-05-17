c3446e6001a1023361d22bbc305aba318eef7f08
Dennis Huebner <dennis.huebner@outlook.com>
2019-05-17 23:02:32 +0200

    created couchbase container + test

---
+++ TestContainers.Tests/ContainerTests/CouchbaseTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using TestContainers.Core.Builders;
+using TestContainers.Core.Containers;
+using Xunit;
+
+namespace TestContainers.Tests.ContainerTests
+{
+
+    public class CouchbaseFixture : IAsyncLifetime
+    {
+
+        public string ConnectionString => Container.ConnectionString;
+
+        private CouchbaseContainer Container { get; }
+
+        public CouchbaseFixture()
+        {
+            Container = new DatabaseContainerBuilder<CouchbaseContainer>()
+                .Begin()
+                .WithImage("couchbase")
+                .WithExposedPorts(8091)
+                .WithEnv(("USERNAME", "admin"))
+                .WithEnv(("PASSWORD", "passwort1234"))
+                .Build();
+        }
+
+        public Task InitializeAsync() => Container.Start();
+
+        public Task DisposeAsync() => Container.Stop();
+    }
+
+    public class CouchbaseTests : IClassFixture<CouchbaseFixture>
+
+    {
+        private readonly Cluster _cluster;
+
+        public CouchbaseTests(CouchbaseFixture fixture) => _cluster =
+            new Cluster(new ClientConfiguration {
+                Servers = new List<Uri>
+                {
+                    new Uri(fixture.ConnectionString)
+
+                }
+            });
+
+        [Fact]
+        public async Task SimpleTest()
+        {
+            string query = "SELECT 1";
+            var createManager = this._cluster.CreateManager();
+            await createManager.CreateBucketAsync("Test");
+            var bucket = await this._cluster.OpenBucketAsync("Test");
+            await bucket.QueryAsync<dynamic>(query);
+
+            Assert.True(true);
+
+            this._cluster.CloseBucket(bucket);
+
+        }
+    }
+}
---
+++ TestContainers/Core/Containers/CouchbaseContainer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using Couchbase.Management;
+using Couchbase.N1QL;
+using Polly;
+
+namespace TestContainers.Core.Containers
+{
+    public class CouchbaseContainer : DatabaseContainer
+    {
+        public const string NAME = "couchbase";
+        public const string IMAGE = "couchbase";
+        public const int COUCHBASE_PORT = 8091;
+
+        public override string DatabaseName => base.DatabaseName ?? _databaseName;
+
+        public override string UserName => base.UserName ?? _username;
+
+        public override string Password => base.Password ?? _password;
+
+        private string _databaseName = "test";
+        private string _username = "admin";
+        private string _password = "passwort1234";
+
+        public CouchbaseContainer() : base()
+        {
+
+        }
+
+        int GetMappedPort(int portNumber) => portNumber;
+
+        public override string ConnectionString => $"http://{GetDockerHostIpAddress()}";
+
+        protected override string TestQueryString => "SELECT 1";
+
+        protected override async Task WaitUntilContainerStarted()
+        {
+            await base.WaitUntilContainerStarted();
+
+            var clientConfiguration = new ClientConfiguration
+            {
+                Servers = new List<Uri> {new Uri(ConnectionString)}
+            };
+
+            var cluster = new Cluster(clientConfiguration);
+            var provisioner = new ClusterProvisioner(cluster, Password, UserName);
+            var createManager = cluster.CreateManager();
+
+            var result = await Policy
+                .TimeoutAsync(TimeSpan.FromMinutes(2))
+                .WrapAsync(Policy
+                    .Handle<CouchbaseResponseException>()
+                    .WaitAndRetryForeverAsync(
+                        iteration => TimeSpan.FromSeconds(10)))
+                .ExecuteAndCaptureAsync(async () =>
+                {
+                    await provisioner.ProvisionEntryPointAsync();
+                    await createManager.CreateBucketAsync(DatabaseName);
+                    var bucket = await cluster.OpenBucketAsync(DatabaseName);
+                    var query = new QueryRequest().Statement(TestQueryString);
+                    await bucket.QueryAsync<dynamic>(query);
+
+                });
+
+            if (result.Outcome == OutcomeType.Failure)
+            {
+                provisioner.Dispose();
+                throw new Exception(result.FinalException.Message);
+            }
+        }
+    }
+}
